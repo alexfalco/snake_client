0498d7b19363058f6c078fbd93192b72728f87b9
alexfalco <afalconer02@gmail.com>
2019-12-04 18:07:35 -0500

    TCP Guarantees Order

--- input.js
+++ input.js
@@ -6,12 +6,14 @@
 // "Move: right" - move left one square (unless facing left)
 
 
+let connection;
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -28,6 +30,15 @@ const setupInput = function() {
 
 
 const handleUserInput = function(key) {
+
+if (key === 'w') { connection.write("Move: up")
+}
+if (key === 's') { connection.write("Move: down")
+}
+if (key === 'd') { connection.write("Move: right")
+}
+if (key === 'a') { connection.write("Move: left")
+}
 
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
